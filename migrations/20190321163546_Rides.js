f9da524b2fe2ece674c77f6889c48985b970c5c6
Matt Basile <matthewbasile2@gmail.com>
2019-03-21 18:21:23 -0400

    Updated migration files to reflex new data structures added an AddUser function to our api/user

--- migrations/20190321163546_Rides.js
+++ migrations/20190321163546_Rides.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('rides', function(tbl){
+        tbl.increments('id').primary();
+        tbl.string('driver_id').unsigned().notNullable().references('id').inTable('drivers').onUpdate('CASCADE');
+        tbl.string('mother_id').unsigned().notNullable().references('id').inTable('mothers').onUpdate('CASCADE');
+        tbl.string("start_village", 500)
+        tbl.json('start_address');
+        tbl.string('destination', 500)
+        tbl.json('destination_address');
+ })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('rides');
 };
--- routes/users-route.js
+++ routes/users-route.js
@@ -1,6 +1,8 @@
 const Users = require('../models/user-model.js');
 module.exports= server =>{
-    server.get('/api/users', getUsersTest)
+    server.get('/api/users', getUsersTest);
+    server.post('/api/users', addUser);
+
 }
 
 const getUsersTest = (req, res) =>{
@@ -10,5 +12,16 @@ const getUsersTest = (req, res) =>{
     })
     .catch(err => {
         res.status(500).json({ message: ` Failed to get Users`, error: err });
+    })
+}
+
+
+const addUser = (req, res) =>{
+    Users.add(req.body)
+    .then(data =>{
+        res.status(201).json(data)
+    })
+    .catch(err => {
+        res.status(500).json({ message: ` Failed to add user`, error: err });
     })
 }
