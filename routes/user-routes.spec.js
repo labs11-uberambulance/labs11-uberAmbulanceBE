28f15278a9fea3d925258aabb13a248839bfb65a
Matt Basile <matthewbasile2@gmail.com>
2019-03-21 13:58:33 -0400

    Added Tests for mother route and updated Migration requirements

--- routes/user-routes.spec.js
+++ routes/user-routes.spec.js
@@ -1,5 +1,8 @@
 const server = require('../server/server.js');
 const request = require('supertest');
+const knex = require('knex');
+const knexConfig = require('../knexfile.js')
+const db = knex(knexConfig.testing);
 
 describe('Test Users Route', () => {
     describe('Get /api/users', () => {
@@ -7,6 +10,12 @@ describe('Test Users Route', () => {
             const res = await request(server).get('/api/users')
             expect(res.text).toEqual("[]")
             expect(res.status).toEqual(200)
+         });
+    });
+    describe('Get /api/mothers', () => {
+        it('Should return 200 ok and empty array', async () => {
+            const moms = await db('mothers').select('google_id')
+            expect(moms).toEqual([])
          });
     });
 });
