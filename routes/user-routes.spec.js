3893cffe4b9f34557c9ee65d49424df89d6c09a2
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-22 15:04:48 -0600

    update tests to expect array (vs. empty array)

--- routes/user-routes.spec.js
+++ routes/user-routes.spec.js
@@ -1,13 +1,12 @@
-const server = require('../server/server.js');
-const request = require('supertest');
+const server = require("../server/server.js");
+const request = require("supertest");
 
-describe('Test Users Route', () => {
-    describe('Get /api/users', () => {
-        it('Should return 200 ok and empty array', async () => {
-            const res = await request(server).get('/api/users')
-            expect(res.text).toEqual("[]")
-            expect(res.status).toEqual(200)
-         });
+describe("Test Users Route", () => {
+  describe("Get /api/users", () => {
+    it("Should return 200 ok and array", async () => {
+      const res = await request(server).get("/api/users");
+      expect(res.body).toEqual(expect.any(Array));
+      expect(res.status).toEqual(200);
     });
-
+  });
 });
