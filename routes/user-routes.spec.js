f17adaf4a180de2183137523fb463fde4a3914f0
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-23 07:46:45 -0600

    add db reset, test get mothers/drivers

--- routes/user-routes.spec.js
+++ routes/user-routes.spec.js
@@ -1,12 +1,36 @@
 const server = require("../server/server.js");
 const request = require("supertest");
+const db = require("../data/dbConfig");
+
+beforeAll(async done => {
+  // reset db
+  await db.migrate.rollback();
+  await db.migrate.latest();
+  await db.seed.run();
+  return done();
+});
 
 describe("Test Users Route", () => {
   describe("Get /api/users", () => {
+    // note: we are seeding >500 users, half "mothers", "half drivers". get response length check assumes seed file is set up this way.
     it("Should return 200 ok and array", async () => {
       const res = await request(server).get("/api/users");
+      console.log(res.body.length); //
       expect(res.body).toEqual(expect.any(Array));
       expect(res.status).toEqual(200);
+      expect(res.body.length).toBeGreaterThan(500);
+    });
+    it("should return 200 and array matching mothers param", async () => {
+      const res = await request(server).get("/api/users/mothers");
+      expect(res.body).toEqual(expect.any(Array));
+      expect(res.status).toEqual(200);
+      expect(res.body.length).toBeGreaterThan(250);
+    });
+    it("should return 200 and array matching drivers param", async () => {
+      const res = await request(server).get("/api/users/drivers");
+      expect(res.body).toEqual(expect.any(Array));
+      expect(res.status).toEqual(200);
+      expect(res.body.length).toBeGreaterThan(250);
     });
   });
 });
