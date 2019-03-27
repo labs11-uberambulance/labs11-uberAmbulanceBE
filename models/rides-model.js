8027e1bd2f73ebeef1667016a157a5c54e2df0e2
Matt Basile <matthewbasile2@gmail.com>
2019-03-27 15:01:33 -0400

    Getting top 6 active drivers and creating a URL

--- models/rides-model.js
+++ models/rides-model.js
@@ -6,11 +6,29 @@ module.exports = {
 
 // Uganda between -1.4, 4.2, faker doesn't play nice with neg nums
 // Uganda between 29.5, 35.5
-async function findDrivers(lat, long){
+async function findDrivers(lat, long, location){
+    
     // const Uganda = //GOOGLE API {lat: -1.4, 4.2} {long: 29.5, 35.5}
     const activeDrivers = await db('drivers').where({"active": true}).select("latitude", "longitude")
-    return activeDrivers
+    var destinations =[]
+    for(i= 0; i<=5; i++){
+        console.log(activeDrivers[i])
+        if(i ===5){
+         const lati = parseInt(activeDrivers[i].latitude)
+         const longi = parseInt(activeDrivers[i].longitude)
+         destinations.push(`${lati}%${longi}&`)
+        }
+        else{
+            const lati = parseInt(activeDrivers[i].latitude)
+         const longi = parseInt(activeDrivers[i].longitude)
+         destinations.push(`${lati}%${longi}%`)
+        }
+      activeDrivers[i]
+    }
+    
     // Send ActiveDrivers Lat and Long to Google to see where they are located
+   const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${lat},${long}&destinations=${destinations.join('')}key=OURAPIKEY`
+   console.log(url);
 }
 
 // async function createRide(data){
