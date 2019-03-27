bcf93532e2929e829710079bd4d5ab38b88ad004
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-27 08:35:34 -0600

    remove login from users table

---
+++ data/migrations/20190320185054_Mothers.js
@@ -0,0 +1,25 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("mothers", function(tbl) {
+    tbl.increments("id").primary();
+    tbl
+      .string("firebase_id")
+      .unsigned()
+      .notNullable()
+      .references("firebase_id")
+      .inTable("users")
+      .onUpdate("CASCADE");
+    tbl.string("address", 500);
+    tbl.string("village", 500);
+    tbl.decimal("latitude", 9, 6);
+    tbl.decimal("longitude", 9, 6);
+    tbl.string("caretaker_name", 500);
+    tbl.date("due_date");
+    tbl.string("hospital", 500);
+    tbl.string("email");
+    tbl.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("mothers");
+};
---
+++ data/seeds/001-users.js
@@ -0,0 +1,42 @@
+const faker = require("faker");
+
+exports.seed = function(knex, Promise) {
+  function createFakeUser(i) {
+    const phone = faker.phone.phoneNumber();
+
+    return {
+      name: faker.name.findName(),
+      firebase_id: faker.random.alphaNumeric(8),
+      phone,
+      user_type: i % 2 ? "mothers" : "drivers"
+    };
+  }
+  const users = [];
+  const numFakes = 502;
+  for (let i = 0; i < numFakes; i++) {
+    users.push(createFakeUser(i));
+  }
+  // edit first 20 users to have a known firebase id, first 10 mothers, next 10 drivers
+  for (let i = 0; i < 10; i++) {
+    users[i] = {
+      ...users[i],
+      name: `mother ${i}`,
+      firebase_id: `mother${i}FIREBASE`,
+      user_type: "mothers"
+    };
+    users[i + 10] = {
+      ...users[i],
+      name: `driver ${i}`,
+      firebase_id: `driver${i}FIREBASE`,
+      user_type: "drivers"
+    };
+  }
+  return (
+    knex("users")
+      // Delete existing entries handled in 000-cleaner.js
+      .then(function() {
+        // Inserts seed entries
+        return knex("users").insert(users);
+      })
+  );
+};
