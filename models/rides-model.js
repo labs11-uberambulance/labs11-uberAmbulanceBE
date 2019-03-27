47053bbad8abd523d02d9522e9a0c886833fe975
Matt Basile <matthewbasile2@gmail.com>
2019-03-27 15:46:50 -0400

    Get rides is creating a URL that will need to use a GET call to return locations but so far its working online.

--- models/rides-model.js
+++ models/rides-model.js
@@ -9,27 +9,30 @@ module.exports = {
 async function findDrivers(lat, long, location){
     
     // const Uganda = //GOOGLE API {lat: -1.4, 4.2} {long: 29.5, 35.5}
-    const activeDrivers = await db('drivers').where({"active": true}).select("latitude", "longitude")
+    const activeDrivers = await db('drivers').where({"active": true}).select("latitude", "longitude").limit(5)
     var destinations =[]
-    for(i= 0; i<=5; i++){
-        console.log(activeDrivers[i])
-        if(i ===5){
-         const lati = parseInt(activeDrivers[i].latitude)
-         const longi = parseInt(activeDrivers[i].longitude)
-         destinations.push(`${lati}%${longi}&`)
-        }
-        else{
-            const lati = parseInt(activeDrivers[i].latitude)
-         const longi = parseInt(activeDrivers[i].longitude)
-         destinations.push(`${lati}%${longi}%`)
-        }
-      activeDrivers[i]
-    }
-    
+    activeDrivers.forEach((local, i) =>{
+        console.log(i)
+        if(i ===4){
+            const lati = parseInt(local.latitude)
+            const longi = parseInt(local.longitude)
+            destinations.push(`${lati}%2C${longi}&`)
+           }
+           else{
+           const lati = parseInt(local.latitude)
+            const longi = parseInt(local.longitude)
+            destinations.push(`${lati}%2C${longi}%7C`)
+           }
+     })
     // Send ActiveDrivers Lat and Long to Google to see where they are located
-   const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${lat},${long}&destinations=${destinations.join('')}key=OURAPIKEY`
-   console.log(url);
+   const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${lat},${long}&destinations=${destinations.join('')}key=${process.env.GOOGLE_MAPS_KEY}`
+   
 }
+// https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=3.5,30.5&destinations=0%2C31%7C0%2C33%7C0%2C32%7C1%2C32%7C
+
+
+// &key=AIzaSyA1tooxvHSDPxbHz2LKMwALSben7D93MLk
+
 
 // async function createRide(data){
 //     if(data.lat && data.long){
