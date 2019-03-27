12d99447e8ff13d8d181b0286077a7799055e7ae
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-27 08:38:40 -0600

    remove extra migrations/seeds dirs

--- data/seeds/000-cleaner.js
+++
@@ -1,9 +0,0 @@
-const knexCleaner = require("knex-cleaner");
-const options = {
-  ignoreTables: ["knex_migrations", "knex_migrations_lock"]
-};
-// using this to avoid using .truncate() .del() trade off in other sees
-// due to limitation of sqlite local + postgres on heroku
-exports.seed = function(knex) {
-  return knexCleaner.clean(knex, options);
-};
