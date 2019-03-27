b8437f60e75ea717ba3f485944fc0225bc27d845
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-26 20:44:09 -0600

    seed mother/driver linked to  firebase_id in users

--- seeds/001-users.js
+++ seeds/001-users.js
@@ -19,6 +19,21 @@ exports.seed = function(knex, Promise) {
   for (let i = 0; i < numFakes; i++) {
     users.push(createFakeUser(i));
   }
+  // edit first 20 users to have a known firebase id, first 10 mothers, next 10 drivers
+  for (let i = 0; i < 10; i++) {
+    users[i] = {
+      ...users[i],
+      name: `mother ${i}`,
+      firebase_id: `mother${i}FIREBASE`,
+      user_type: "mothers"
+    };
+    users[i + 10] = {
+      ...users[i],
+      name: `driver ${i}`,
+      firebase_id: `driver${i}FIREBASE`,
+      user_type: "drivers"
+    };
+  }
   return (
     knex("users")
       // Delete existing entries handled in 000-cleaner.js
