5e1d9f0dd781e15ef534c37bb81c57259fc42d46
Matt Basile <matthewbasile2@gmail.com>
2019-04-15 12:58:02 -0400

    twilio modifications

--- routes/twilio.js
+++ routes/twilio.js
@@ -3,18 +3,6 @@ const router = express.Router();
 const twilio = require('../services/twilio');
 const { handle_incoming_messages } = require('../middleware/twilio');
 
-
-router.get('/text-me', (req, res, next) => {
-    twilio.messages.create({
-        body: 'Hello Orlando, from Node',
-        to: process.env.MY_NUMBER,
-        from: '+19179709371'
-    }).then(message => {
-        res.status(200).json({id: message.sid})
-    });
-})
-
-
 router.post('/sms/receiver', handle_incoming_messages)
 
 
